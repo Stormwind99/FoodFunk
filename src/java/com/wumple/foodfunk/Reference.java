af97f2769f293c0d77306a6a7d90e79bcf367447
Jason Spangler <jason.spangler@descendentstudios.com>
2018-07-05 01:16:55 -0500

    Switch to @ based Gradle replacement vars

--- src/java/com/wumple/foodfunk/Reference.java
+++ src/java/com/wumple/foodfunk/Reference.java
@@ -5,13 +5,13 @@ public class Reference
     // Mod Constants
 
     // /*
-    public static final String MOD_ID = "GRADLE:MOD_ID";
-    public static final String MOD_NAME = "GRADLE:MOD_NAME";
-    public static final String BUILD = "GRADLE:BUILD";
-    public static final String MOD_VERSION = "GRADLE:VERSION";
-    public static final String INTERNALVERSION = "GRADLE:INTERNALVERSIONMAJOR-" + BUILD;
-    public static final String DEPENDENCIES = "GRADLE:DEPENDENCIES";
-    public static final String UPDATEJSON = "GRADLE:UPDATEJSON";
+    public static final String MOD_ID = "@MOD_ID@";
+    public static final String MOD_NAME = "@MOD_NAME@";
+    public static final String BUILD = "@BUILD@";
+    public static final String MOD_VERSION = "@VERSION@";
+    public static final String INTERNALVERSION = "@INTERNALVERSIONMAJOR@-" + BUILD;
+    public static final String DEPENDENCIES = "@DEPENDENCIES@";
+    public static final String UPDATEJSON = "@UPDATEJSON@";
     // */
 
     /*
