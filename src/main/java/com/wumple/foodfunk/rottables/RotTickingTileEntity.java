2d2639934c2ad035a21b7996282607c750719103
Jason Spangler <jasons@wumple.com>
2018-07-31 14:29:54 -0500

    RotTickingTileEntity support for IThing.forceUpdate

--- src/main/java/com/wumple/foodfunk/rottables/RotTickingTileEntity.java
+++ src/main/java/com/wumple/foodfunk/rottables/RotTickingTileEntity.java
@@ -1,9 +1,14 @@
 package com.wumple.foodfunk.rottables;
 
+import javax.annotation.Nullable;
+
 import com.wumple.foodfunk.capability.rot.IRot;
 import com.wumple.util.adapter.TUtil;
 import com.wumple.util.tileentity.placeholder.TickingTileEntityPlaceholder;
 
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.play.server.SPacketUpdateTileEntity;
 import net.minecraft.world.World;
 
 public class RotTickingTileEntity extends TickingTileEntityPlaceholder
@@ -15,5 +20,25 @@ public class RotTickingTileEntity extends TickingTileEntityPlaceholder
         {
             cap.evaluate(world, TUtil.to(this));
         }
+    }
+
+    @Override
+    @Nullable
+    public SPacketUpdateTileEntity getUpdatePacket()
+    {
+        return new SPacketUpdateTileEntity(this.pos, 3, this.getUpdateTag());
+    }
+
+    @Override
+    public NBTTagCompound getUpdateTag()
+    {
+        return this.writeToNBT(new NBTTagCompound());
+    }
+
+    @Override
+    public void onDataPacket(NetworkManager net, SPacketUpdateTileEntity pkt)
+    {
+        super.onDataPacket(net, pkt);
+        handleUpdateTag(pkt.getNbtCompound());
     }
 }
