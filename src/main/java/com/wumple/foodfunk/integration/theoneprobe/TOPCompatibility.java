0dbf7840855fe2604a7510b09324199cc4f07251
Jason Spangler <jasons@wumple.com>
2018-08-14 17:44:38 -0500

    Defensive symbol separation for Hwyla/Waila and TheOneProbe support

--- src/main/java/com/wumple/foodfunk/integration/theoneprobe/TOPCompatibility.java
+++ src/main/java/com/wumple/foodfunk/integration/theoneprobe/TOPCompatibility.java
@@ -1,79 +1,15 @@
 package com.wumple.foodfunk.integration.theoneprobe;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Function;
-
-import javax.annotation.Nullable;
-
-import com.wumple.foodfunk.FoodFunk;
-import com.wumple.foodfunk.capability.preserving.IPreserving;
-import com.wumple.foodfunk.capability.rot.IRot;
-import com.wumple.foodfunk.configuration.ConfigContainer;
-import com.wumple.util.tooltip.ITooltipProvider;
-import com.wumple.util.tooltip.TooltipUtils;
-
-import mcjty.theoneprobe.api.IProbeHitData;
-import mcjty.theoneprobe.api.IProbeInfo;
-import mcjty.theoneprobe.api.IProbeInfoProvider;
-import mcjty.theoneprobe.api.ITheOneProbe;
-import mcjty.theoneprobe.api.ProbeMode;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.tileentity.TileEntity;
-import net.minecraft.world.World;
 import net.minecraftforge.fml.common.event.FMLInterModComms;
 
 public class TOPCompatibility
 {
-
     private static boolean registered;
 
     public static void register()
     {
         if (registered) { return; }
         registered = true;
-        FMLInterModComms.sendFunctionMessage("theoneprobe", "getTheOneProbe", "com.wumple.foodfunk.integration.theoneprobe.TOPCompatibility$GetTheOneProbe");
-    }
-
-    public static class GetTheOneProbe implements Function<ITheOneProbe, Void>
-    {
-
-        public static ITheOneProbe probe;
-
-        @Nullable
-        @Override
-        public Void apply(ITheOneProbe theOneProbe)
-        {
-            probe = theOneProbe;
-            FoodFunk.logger.info("Enabled support for The One Probe");
-            probe.registerProvider(new IProbeInfoProvider()
-            {
-                @Override
-                public String getID()
-                {
-                    return "foodfunk:default";
-                }
-                
-                protected void tipsToProbe(IProbeInfo probeInfo, List<String> tips)
-                {
-                    for (String tip : tips)
-                    {
-                        probeInfo.horizontal().text(tip);
-                    }
-                }
-
-                @Override
-                public void addProbeInfo(ProbeMode mode, IProbeInfo probeInfo, EntityPlayer player, World world, IBlockState blockState, IProbeHitData data)
-                {
-                    TileEntity te = world.getTileEntity(data.getPos());
-                    List<String> tips = new ArrayList<String>();
-                    ITooltipProvider[] providers = { IRot.getMyCap(te), IPreserving.getMyCap(te) };
-                    TooltipUtils.doTooltip(providers, null, player, ConfigContainer.zdebugging.debug, tips);
-                    tipsToProbe(probeInfo, tips);
-                }
-            });
-            return null;
-        }
+        FMLInterModComms.sendFunctionMessage("theoneprobe", "getTheOneProbe", "com.wumple.foodfunk.integration.theoneprobe.TOPProvider");
     }
 }
--- src/main/java/com/wumple/foodfunk/integration/waila/WailaCompatibility.java
+++ src/main/java/com/wumple/foodfunk/integration/waila/WailaCompatibility.java
@@ -1,93 +1,20 @@
 package com.wumple.foodfunk.integration.waila;
 
-import java.util.List;
-
-import com.wumple.foodfunk.FoodFunk;
-import com.wumple.foodfunk.capability.preserving.IPreserving;
-import com.wumple.foodfunk.capability.rot.IRot;
-import com.wumple.foodfunk.coldchest.TileEntityColdChest;
-import com.wumple.foodfunk.configuration.ConfigContainer;
-import com.wumple.util.placeholder.TileEntityPlaceholder;
-import com.wumple.util.tooltip.ITooltipProvider;
-import com.wumple.util.tooltip.TooltipUtils;
-
-import mcp.mobius.waila.api.IWailaConfigHandler;
-import mcp.mobius.waila.api.IWailaDataAccessor;
-import mcp.mobius.waila.api.IWailaDataProvider;
-import mcp.mobius.waila.api.IWailaRegistrar;
-import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 import net.minecraftforge.fml.common.event.FMLInterModComms;
 
-public class WailaCompatibility implements IWailaDataProvider
+public class WailaCompatibility 
 {
-
-    public static final WailaCompatibility INSTANCE = new WailaCompatibility();
-
-    private WailaCompatibility()
-    {
-    }
-
     private static boolean registered;
-    private static boolean loaded;
-
-    public static void load(IWailaRegistrar registrar)
-    {
-        FoodFunk.logger.info("Enabled support for Waila / Hwyla");
-        if (!registered)
-        {
-            throw new RuntimeException("Please register this handler using the provided method.");
-        }
-        if (!loaded)
-        {
-            registrar.registerBodyProvider(INSTANCE, TileEntityPlaceholder.class);
-            registrar.registerBodyProvider(INSTANCE, TileEntityColdChest.class);
-            loaded = true;
-        }
-    }
 
     public static void register()
     {
         if (registered) { return; }
         registered = true;
-        FMLInterModComms.sendMessage("waila", "register", "com.wumple.foodfunk.integration.waila.WailaCompatibility.load");
+        FMLInterModComms.sendMessage("waila", "register", "com.wumple.foodfunk.integration.waila.WailaDataProvider.load");
     }
-
-    @Override
-    public NBTTagCompound getNBTData(EntityPlayerMP player, TileEntity te, NBTTagCompound tag, World world, BlockPos pos)
+    
+    public static boolean getRegistered()
     {
-        return tag;
+        return registered;
     }
-
-    @Override
-    public ItemStack getWailaStack(IWailaDataAccessor accessor, IWailaConfigHandler config)
-    {
-        return null;
-    }
-
-    @Override
-    public List<String> getWailaHead(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config)
-    {
-        return currenttip;
-    }
-
-    @Override
-    public List<String> getWailaBody(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config)
-    {
-        TileEntity te = accessor.getWorld().getTileEntity(accessor.getPosition());
-        ITooltipProvider[] providers = { IRot.getMyCap(te), IPreserving.getMyCap(te) };
-        TooltipUtils.doTooltip(providers, null, accessor.getPlayer(), ConfigContainer.zdebugging.debug, currenttip);
-        return currenttip;
-    }
-
-    @Override
-    public List<String> getWailaTail(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config)
-    {
-        return currenttip;
-    }
-
 }
